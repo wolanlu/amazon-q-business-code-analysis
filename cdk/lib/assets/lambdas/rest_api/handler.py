554e9ba9a74914f55ab58307f85c7f5720610925
Lukasz Wolanski <101870655+wolanlu@users.noreply.github.com>
2024-03-13 13:13:20 +0100

    basic pushEvent validation

--- cdk/lib/assets/lambdas/rest_api/handler.py
+++ cdk/lib/assets/lambdas/rest_api/handler.py
@@ -1,5 +1,6 @@
 from aws_lambda_powertools import Logger, Tracer
 from aws_lambda_powertools.event_handler import APIGatewayRestResolver
+from aws_lambda_powertools.event_handler.exceptions import BadRequestError
 from aws_lambda_powertools.logging import correlation_paths
 from aws_lambda_powertools.utilities.typing import LambdaContext
 
@@ -8,16 +9,32 @@ logger = Logger()
 app = APIGatewayRestResolver()
 
 
-@app.post("/todos")
+@app.post("/webhooks/push")
 @tracer.capture_method
-def create_todo():
-    todo_data: dict = app.current_event.json_body  # deserialize json str to dict
-    logger.info("todos input", extra={"request": todo_data})
+def push():
+    push_event: dict = app.current_event.json_body  # deserialize json str to dict
+    if 'type' not in push_event or push_event['type'] != 'PushEvent':
+        raise BadRequestError("Not a push event")
 
-    return {"todo": "todo"}
+    if 'repository' not in push_event or 'url' not in push_event['repository']:
+        raise BadRequestError("Missing repository")
+    repo_url = push_event['repository']['url']
+
+    if 'payload' not in push_event:
+        raise BadRequestError("Missing payload")
+    if 'head' not in push_event['payload']:
+        raise BadRequestError("Missing head")
+    head = push_event['payload']['head']
+
+    if 'ref' not in push_event['payload']:
+        raise BadRequestError("Missing ref")
+    ref = push_event['payload']['ref']
+
+    logger.info("Starting batch", extra={"repo_url": repo_url, "head": head, "ref": ref})
+
+    return {"repo_url": repo_url, "head": head, "ref": ref}
 
 
-# You can continue to use other utilities just as before
 @logger.inject_lambda_context(correlation_id_path=correlation_paths.API_GATEWAY_REST)
 @tracer.capture_lambda_handler
 def lambda_handler(event: dict, context: LambdaContext) -> dict:
