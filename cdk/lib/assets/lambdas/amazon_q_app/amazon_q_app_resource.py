b9eac0a93aeb16cb9486a3f69c3e0a7a21f38c13
Lukasz Wolanski <101870655+wolanlu@users.noreply.github.com>
2024-03-20 15:39:17 +0100

    add sdk call to create web experience (in the same way as using AWS Console)

--- cdk/lib/assets/lambdas/amazon_q_app/amazon_q_app_resource.py
+++ cdk/lib/assets/lambdas/amazon_q_app/amazon_q_app_resource.py
@@ -3,78 +3,79 @@ import os
 import time
 import json
 import datetime
-from botocore.auth import SigV4Auth
-from botocore.awsrequest import AWSRequest
-import botocore.session
 
 amazon_q = boto3.client('qbusiness')
 
-def on_event(event, context):
-  q_app_name = os.environ['Q_APP_NAME']
-  q_app_role_arn = os.environ['Q_APP_ROLE_ARN']
-  # physical_id = "PhysicalIdAmazonQCodeAnalysisApp"
 
-  print(json.dumps(event))
-  request_type = event['RequestType']
-  if request_type == 'Create': return on_create(event,  
-                                                q_app_name=q_app_name,
-                                                q_app_role_arn=q_app_role_arn,
-                                                )
-  physical_id = event['PhysicalResourceId']
-  if request_type == 'Update': return on_update(event, physical_id=physical_id)
-  if request_type == 'Delete': return on_delete(event, physical_id=physical_id)
-  raise Exception("Invalid request type: %s" % request_type)
+def on_event(event, context):
+    q_app_name = os.environ['Q_APP_NAME']
+    q_app_role_arn = os.environ['Q_APP_ROLE_ARN']
+    # physical_id = "PhysicalIdAmazonQCodeAnalysisApp"
+
+    print(json.dumps(event))
+    request_type = event['RequestType']
+    if request_type == 'Create': return on_create(event,
+                                                  q_app_name=q_app_name,
+                                                  q_app_role_arn=q_app_role_arn,
+                                                  )
+    physical_id = event['PhysicalResourceId']
+    if request_type == 'Update': return on_update(event, physical_id=physical_id)
+    if request_type == 'Delete': return on_delete(event, physical_id=physical_id)
+    raise Exception("Invalid request type: %s" % request_type)
 
 
 def on_create(event, q_app_name, q_app_role_arn):
-  props = event["ResourceProperties"]
-  print("create new resource with props %s" % props)
+    props = event["ResourceProperties"]
+    print("create new resource with props %s" % props)
 
-  # Create Amazon Q App
-  amazon_q_app_id = create_q_app(q_app_name=q_app_name,
-                                 role_arn=q_app_role_arn)
-  
-  # Create Q Index
-  amazon_q_index_id = create_q_index(q_app_name=q_app_name,
-                                     amazon_q_app_id=amazon_q_app_id)
+    # Create Amazon Q App
+    amazon_q_app_id = create_q_app(q_app_name=q_app_name,
+                                   role_arn=q_app_role_arn)
 
-  # Create Q Retriever
-  amazon_q_retriever_id = create_q_retriever(
-    q_app_name=q_app_name,
-    amazon_q_app_id=amazon_q_app_id,
-    amazon_q_index_id=amazon_q_index_id,
-    q_app_role_arn=q_app_role_arn,
-  )
-  
-  return { 
-    'PhysicalResourceId': amazon_q_app_id, 
-    'Data': { 
-        'AmazonQAppId': amazon_q_app_id, 
-        'AmazonQIndexId': amazon_q_index_id, 
-        'AmazonQRetrieverId': amazon_q_retriever_id 
-      }
-  }
+    # Create Q Index
+    amazon_q_index_id = create_q_index(q_app_name=q_app_name,
+                                       amazon_q_app_id=amazon_q_app_id)
+
+    # Create Q Retriever
+    amazon_q_retriever_id = create_q_retriever(
+        q_app_name=q_app_name,
+        amazon_q_app_id=amazon_q_app_id,
+        amazon_q_index_id=amazon_q_index_id,
+        q_app_role_arn=q_app_role_arn,
+    )
+
+    web_experience_id = create_q_web_experience(amazon_q_app_id=amazon_q_app_id, q_app_name=q_app_name)
+
+    return {
+        'PhysicalResourceId': amazon_q_app_id,
+        'Data': {
+            'AmazonQAppId': amazon_q_app_id,
+            'AmazonQIndexId': amazon_q_index_id,
+            'AmazonQRetrieverId': amazon_q_retriever_id,
+            'AmazonQWebExperienceId': web_experience_id
+        }
+    }
 
 
 def on_update(event, physical_id):
-  # physical_id = event["PhysicalResourceId"]
-  props = event["ResourceProperties"]
-  print("update resource %s with props %s" % (physical_id, props))
+    # physical_id = event["PhysicalResourceId"]
+    props = event["ResourceProperties"]
+    print("update resource %s with props %s" % (physical_id, props))
 
-  return { 'PhysicalResourceId': physical_id } 
+    return {'PhysicalResourceId': physical_id}
 
 
 def on_delete(event, physical_id):
-  # physical_id = event["PhysicalResourceId"]
-  print("delete resource %s" % physical_id)
-  delete_amazon_q_app(q_app_id=physical_id)
+    # physical_id = event["PhysicalResourceId"]
+    print("delete resource %s" % physical_id)
+    delete_amazon_q_app(q_app_id=physical_id)
 
-  return { 'PhysicalResourceId': physical_id } 
+    return {'PhysicalResourceId': physical_id}
 
 
 def create_q_app(q_app_name, role_arn):
-  import datetime
-  response = amazon_q.create_application(
+    import datetime
+    response = amazon_q.create_application(
         attachmentsConfiguration={
             'attachmentsControlMode': 'ENABLED'
         },
@@ -82,9 +83,10 @@ def create_q_app(q_app_name, role_arn):
         description=f"{q_app_name} created with Cloudformation on {datetime.datetime.now().strftime('%Y-%m-%d %H:%M:%S')}",
         roleArn=role_arn,
     )
-  amazon_q_app_id = response["applicationId"]
+    amazon_q_app_id = response["applicationId"]
 
-  return amazon_q_app_id
+    return amazon_q_app_id
+
 
 def create_q_index(q_app_name, amazon_q_app_id):
     response = amazon_q.create_index(
@@ -98,17 +100,18 @@ def create_q_index(q_app_name, amazon_q_app_id):
     index_id = response["indexId"]
 
     while True:
-      response = amazon_q.get_index(
-      applicationId=amazon_q_app_id,
-      indexId=index_id,
-      )
-      status = response.get('status')
-      print(f"Creat index status {status}")
-      if status == 'ACTIVE':
-          break
-      time.sleep(10)
+        response = amazon_q.get_index(
+            applicationId=amazon_q_app_id,
+            indexId=index_id,
+        )
+        status = response.get('status')
+        print(f"Creat index status {status}")
+        if status == 'ACTIVE':
+            break
+        time.sleep(10)
 
     return index_id
+
 
 def create_q_retriever(q_app_name, amazon_q_app_id, amazon_q_index_id, q_app_role_arn):
     response = amazon_q.create_retriever(
@@ -126,9 +129,20 @@ def create_q_retriever(q_app_name, amazon_q_app_id, amazon_q_index_id, q_app_rol
 
     return retriever_id
 
+
 def delete_amazon_q_app(q_app_id):
-  response = amazon_q.delete_application(
+    response = amazon_q.delete_application(
         applicationId=q_app_id
     )
 
-  return response
+    return response
+
+
+def create_q_web_experience(amazon_q_app_id, q_app_name):
+    response = amazon_q.create_web_experience(
+        applicationId=amazon_q_app_id,
+        title=q_app_name
+    )
+    web_experience_id = response["webExperienceId"]
+
+    return web_experience_id
